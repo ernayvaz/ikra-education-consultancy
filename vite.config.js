3add538b39a7690632da3258999dff3f24a4ff69
Eren <95831938+ernayvaz@users.noreply.github.com>
2025-07-19 18:50:51 +0300

    Disable modulepreload polyfill

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    // Disable Vite's modulepreload polyfill to avoid invalid fetches on extension links
+    polyfillModulePreload: false,
+  },
 }) 
